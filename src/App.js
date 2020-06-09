dd734aa8767b095204ebc590f83e7cd201f7d885
Manami Ueda <manamiueda1@gmail.com>
2020-06-09 19:53:15 -0400

    editing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <h1
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         MANAMI <br></br> UEDA
+  //       </h1>
+  //     </header>
+  //   </div>
+  // );
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>SOFTWARE ENGINEER, WEB DEVELOPER & YOGI</h3>
+        <h1 className="Name">
+          MANAMI <br></br> UEDA
+        </h1>
+        <img src="portfolio_img.jpg" alt="portfolio_shot"></img>
       </header>
     </div>
   );
