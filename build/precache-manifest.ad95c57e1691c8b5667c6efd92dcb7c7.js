a642be6396da477b5b156073d050ea99f03a3c79
Manami Ueda <manamiueda1@gmail.com>
2020-06-20 00:19:52 -0400

    small change

---
+++ build/precache-manifest.ad95c57e1691c8b5667c6efd92dcb7c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383415f49eb8a995916c625180e48479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae3a54b9d711beaaaff5",
+    "url": "/static/css/main.5f92830e.chunk.css"
+  },
+  {
+    "revision": "57de0d6567f799eb1ec6",
+    "url": "/static/js/2.a832589d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a832589d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3a54b9d711beaaaff5",
+    "url": "/static/js/main.cd14e36c.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "c420da08b76f3d225cb7832833acf65b",
+    "url": "/static/media/car-ify-image.c420da08.png"
+  },
+  {
+    "revision": "bc24c75a68e35670bcf6948c3ee99e81",
+    "url": "/static/media/fit-minded-iphone-image.bc24c75a.png"
+  },
+  {
+    "revision": "3480db5eabd3ef35cf349caa44c5171e",
+    "url": "/static/media/github-logo.3480db5e.png"
+  },
+  {
+    "revision": "03c590727fa42b2a5744f1b5709057da",
+    "url": "/static/media/image-to-voice.03c59072.png"
+  },
+  {
+    "revision": "335a6e08ce44a3ca03c5698b0d9eb38b",
+    "url": "/static/media/portfolio_img.335a6e08.jpg"
+  }
+]);
--- src/Resume.js
+++ src/Resume.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import resumePdf from "./images/Manami_Ueda_Resume_website.pdf";
 
 function Resume() {
   return (
@@ -7,16 +8,12 @@ function Resume() {
       <h1>Resume</h1>
       <div class="Bar"></div>
       <div className="Resume-link">
-        <a
-          href="https://manami-stackathon.firebaseapp.com/"
-          target="_blank"
-          className="Try-demo"
-          type="button"
-        >
+        <a href={resumePdf} target="_blank" className="Try-demo" type="button">
           View Resume
         </a>
         <a
-          href="https://github.com/manamiueda1/stackathon"
+          href={resumePdf}
+          download
           target="_blank"
           className="View-code"
           type="button"
